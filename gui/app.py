4cd4706ea9ccd0f104ce348a419931116e6b9c6e
ballooner <26182964+ballooner@users.noreply.github.com>
2026-03-30 15:41:32 -0400

    Created basic file structure and .gitignore

---
+++ gui/app.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import ttk
+
+def main():
+    root = Tk()
+    ttk.Button(root, text="Hello, world!").grid()
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
