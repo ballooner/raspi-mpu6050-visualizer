6772672cd113c88836beee26940f429ee2fbbed6
ballooner <26182964+ballooner@users.noreply.github.com>
2026-03-31 10:25:02 -0400

    Started data visualization using Dash/Plotly and imported a fake data set for testing

--- gui/app.py
+++ gui/app.py
@@ -1,10 +1,22 @@
-from tkinter import *
-from tkinter import ttk
+from dash import Dash, html, dcc, callback, Output, Input
+import plotly.express as px
+import pandas as pd
 
-def main():
-    root = Tk()
-    ttk.Button(root, text="Hello, world!").grid()
-    root.mainloop()
+df = pd.read_csv(r"../data/gyroscope_data.csv")
 
-if __name__ == "__main__":
-    main()
+app = Dash()
+
+fig = figure=px.line(df, x="t",y="x", title="Gyroscope x")
+fig.update_layout(
+    xaxis_title="Time (s)",
+    yaxis_title="Deg/s"
+)
+
+# Requires Dash 2.17.0 or later
+app.layout = [
+    html.H1(children='Gyrometer data', style={'textAlign':'center'}),
+    dcc.Graph(figure=fig)
+]
+
+if __name__ == '__main__':
+    app.run(debug=True)
