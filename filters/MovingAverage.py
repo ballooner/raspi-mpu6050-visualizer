e522d4b4b29da8ba23df241d580767cb692e494c
Home Desktop-WSL Ubuntu <26182964+ballooner@users.noreply.github.com>
2026-04-01 17:45:45 -0400

    Created file for moving average filter

---
+++ filters/MovingAverage.py
@@ -0,0 +1,11 @@
+import sys
+
+def main():
+    # Make sure file argument is provided
+    if (len(sys.argv) < 2):
+        print("File not provided")
+        return
+
+
+if __name__ == "__main__":
+    main()
