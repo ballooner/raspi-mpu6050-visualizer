26993b902b779cf707b8f88b663582946a6fd890
ballooner <26182964+ballooner@users.noreply.github.com>
2026-04-06 17:36:53 -0400

    Added visualization of every axis of gyrometer data

--- visualization/app.py
+++ visualization/app.py
@@ -1,22 +1,47 @@
 from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
-
-df = pd.read_csv(r"../data/Moving/filteredData.csv")
+from util.constants import DATA_DIR
+import os
 
 app = Dash()
 
-fig = figure=px.line(df, x="t",y="x", title="Gyroscope x")
-fig.update_layout(
-    xaxis_title="Time (s)",
-    yaxis_title="Deg/s"
-)
-
-# Requires Dash 2.17.0 or later
 app.layout = [
     html.H1(children='Gyrometer data', style={'textAlign':'center'}),
-    dcc.Graph(figure=fig)
+    dcc.Dropdown(os.listdir(DATA_DIR), "", id="data-folders"),
+    dcc.Graph(id="gyro-x"),
+    dcc.Graph(id="gyro-y"),
+    dcc.Graph(id="gyro-z"),
 ]
+
+@callback(
+    Output("gyro-x", "figure"),
+    Output("gyro-y", "figure"),
+    Output("gyro-z", "figure"),
+    Input("data-folders", "value"),
+)
+def updateGraphs(dataFolder):
+    if (dataFolder == ""):
+        return [px.line(), px.line(), px.line()]
+
+    dataFile = DATA_DIR / dataFolder / "rawData.csv"
+    data = pd.read_csv(dataFile)
+
+    xFig = createFigure(data, "t", "x", "Gyro X Measurements")
+    yFig = createFigure(data, "t", "y", "Gyro Y Measurements")
+    zFig = createFigure(data, "t", "z", "Gyro Z Measurements")
+
+    return xFig, yFig, zFig
+    
+def createFigure(dataFrame, xHeader, yHeader, graphTitle):
+    fig = px.line(dataFrame, x=xHeader,y=yHeader, title=graphTitle)
+    fig.update_layout(
+        xaxis_title="Time (s)",
+        yaxis_title=yHeader + "Deg/s"
+    )
+
+    return fig
+
 
 if __name__ == '__main__':
     app.run(debug=True)
